ab408d96d93340df7f4db4367b43c60ed7ee3136
KR15hna7 <51372040+KR15hna7@users.noreply.github.com>
2023-05-23 15:27:56 -0400

    Update createUser.sh

--- createUser.sh
+++ createUser.sh
@@ -6,7 +6,7 @@ echo "admin" | sudo -S adduser --disabled-password --gecos "" svc_devops_deploy
 echo "admin" | sudo -S usermod -aG sudo svc_devops_deploy
 
 # Configure sudoers file to allow passwordless sudo for the new user
-echo "$username ALL=(ALL) NOPASSWD:ALL" | sudo -S tee /etc/sudoers.d/svc_devops_deploy
+echo "svc_devops_deploy ALL=(ALL) NOPASSWD:ALL" | sudo -S tee /etc/sudoers.d/svc_devops_deploy
 
 # Set ownership and permissions for the sudoers file
 echo "admin" | sudo -S chmod 0440 /etc/sudoers.d/svc_devops_deploy
